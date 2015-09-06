1c138257c17715eed319129af3b479111003e3a6
好个飞飞 <haogefeifei@qq.com>
2015-09-06 10:54:53 +0800

    初始化项目

---
+++ app/src/test/java/haogefeifei/com/odoo_mobile_building/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package haogefeifei.com.odoo_mobile_building;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
