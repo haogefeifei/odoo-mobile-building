474c94c6d112b340ac357b4a28a594d93cf081ca
好个飞飞 <haogefeifei@qq.com>
2015-09-12 00:26:17 +0800

    客户侧滑标签过滤view

--- app/src/main/java/com/haogefeifei/odoo/ui/activity/PartnerListActivity.java
+++ app/src/main/java/com/haogefeifei/odoo/ui/activity/PartnerListActivity.java
@@ -1,12 +1,15 @@
 package com.haogefeifei.odoo.ui.activity;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import com.haogefeifei.odoo.R;
 import com.haogefeifei.odoo.adapter.LargeAdapter;
@@ -17,12 +20,14 @@ public class PartnerListActivity extends BaseActivity {
 
     public static final String TAG = "PartnerListActivity";
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partner_list);
 
-        initView();
+        setupView();
     }
 
     @Override
@@ -38,6 +43,9 @@ public class PartnerListActivity extends BaseActivity {
             case android.R.id.home:
                 finish();
                 break;
+            case R.id.action_filter:
+                mDrawerLayout.openDrawer(GravityCompat.END);
+                break;
             default:
                 break;
         }
@@ -45,7 +53,7 @@ public class PartnerListActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void initView(){
+    private void setupView(){
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -53,10 +61,22 @@ public class PartnerListActivity extends BaseActivity {
         final ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        LinearLayout navigationView = (LinearLayout) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
         RecyclerView recyclerView=(RecyclerView)findViewById(R.id.rv_partner_list);
         recyclerView.setAdapter(new LargeAdapter(this));
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         FastScroller fastScroller=(FastScroller)findViewById(R.id.fast_scroller);
         fastScroller.setRecyclerView(recyclerView);
+    }
+
+    private void setupDrawerContent(LinearLayout navigationView) {
+
+
     }
 }
