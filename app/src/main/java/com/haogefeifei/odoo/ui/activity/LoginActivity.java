ae56279a8e7091a7294560ad31f716f4a643f55f
好个飞飞 <haogefeifei@qq.com>
2015-09-17 22:09:30 +0800

    登录 todo

--- app/src/main/java/com/haogefeifei/odoo/ui/activity/LoginActivity.java
+++ app/src/main/java/com/haogefeifei/odoo/ui/activity/LoginActivity.java
@@ -2,18 +2,33 @@ package com.haogefeifei.odoo.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.haogefeifei.odoo.R;
 import com.haogefeifei.odoo.ui.activity.base.BaseActivity;
+import com.haogefeifei.odoo.view.InputMethodRelativeLayout;
 
-public class LoginActivity extends BaseActivity {
+/**
+ * 登录页面
+ */
+public class LoginActivity extends BaseActivity implements InputMethodRelativeLayout.OnSizeChangedListenner {
 
     public static final String TAG = "LoginActivity";
 
-    private AppCompatButton btnLogin ;
+    private AppCompatButton btnLogin;
+    private EditText edtAccount;
+    private EditText edtPassWord;
+    private EditText edtUrl;
+    private ImageView imgLogo;
+    private TextView txtUseUrl;
+    private TextInputLayout layoutInputUrl;
+    private InputMethodRelativeLayout layoutLoginPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +39,25 @@ public class LoginActivity extends BaseActivity {
         setupView();
     }
 
-    private void setupView(){
+    private void setupView() {
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        btnLogin = (AppCompatButton)findViewById(R.id.btn_login);
+        imgLogo = (ImageView) findViewById(R.id.img_logo);
+        imgLogo.setFocusable(true);
+        imgLogo.setFocusableInTouchMode(true);
+
+        txtUseUrl = (TextView) findViewById(R.id.link_use_url);
+        layoutInputUrl = (TextInputLayout) findViewById(R.id.layout_input_url);
+        layoutLoginPage = (InputMethodRelativeLayout)findViewById(R.id.login_page);
+        layoutLoginPage.setOnSizeChangedListenner(this);
+
+        edtAccount = (EditText) findViewById(R.id.input_account);
+        edtPassWord = (EditText) findViewById(R.id.input_password);
+        edtUrl = (EditText) findViewById(R.id.input_url);
+
+        btnLogin = (AppCompatButton) findViewById(R.id.btn_login);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +66,32 @@ public class LoginActivity extends BaseActivity {
                 LoginActivity.this.finish();
             }
         });
+
+        txtUseUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (txtUseUrl.getText().equals(getResources().getString(R.string.txt_use_url))) {
+                    txtUseUrl.setText(getResources().getString(R.string.txt_default_url));
+                    layoutInputUrl.setVisibility(View.VISIBLE);
+                } else {
+                    txtUseUrl.setText(getResources().getString(R.string.txt_use_url));
+                    layoutInputUrl.setVisibility(View.GONE);
+                }
+
+            }
+        });
     }
 
+
+    @Override
+    public void onSizeChange(boolean flag, int w, int h) {
+        if (flag) {//键盘弹出时
+            layoutLoginPage.setPadding(0, -10, 0, 0);
+            imgLogo.setVisibility(View.GONE);
+        } else { //键盘隐藏时
+            layoutLoginPage.setPadding(0, 0, 0, 0);
+            imgLogo.setVisibility(View.VISIBLE);
+        }
+    }
 }
---
+++ app/src/main/java/com/haogefeifei/odoo/view/InputMethodRelativeLayout.java
@@ -0,0 +1,85 @@
+package com.haogefeifei.odoo.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Display;
+import android.widget.RelativeLayout;
+
+import com.haogefeifei.odoo.utils.LogOldUtil;
+
+/**
+ * @author junjun
+ *         自定义布局解决键盘弹出挡住输入框的问题
+ */
+public class InputMethodRelativeLayout extends RelativeLayout {
+    private int width;
+    protected OnSizeChangedListenner onSizeChangedListenner;
+    private boolean sizeChanged = false; //变化的标志
+    private int height;
+    private int screenWidth; //屏幕宽度
+    private int screenHeight; //屏幕高度
+
+    public InputMethodRelativeLayout(Context paramContext,
+                                     AttributeSet paramAttributeSet) {
+        super(paramContext, paramAttributeSet);
+        Display localDisplay = ((Activity) paramContext).getWindowManager()
+                .getDefaultDisplay();
+        this.screenWidth = localDisplay.getWidth();
+        this.screenHeight = localDisplay.getHeight();
+    }
+
+    public InputMethodRelativeLayout(Context paramContext,
+                                     AttributeSet paramAttributeSet, int paramInt) {
+        super(paramContext, paramAttributeSet, paramInt);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        this.width = widthMeasureSpec;
+        this.height = heightMeasureSpec;
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    public void onSizeChanged(int w, int h, int oldw,
+                              int oldh) {
+
+        //监听不为空、宽度不变、当前高度与历史高度不为0
+        if ((this.onSizeChangedListenner != null) && (w == oldw) && (oldw != 0)
+                && (oldh != 0)) {
+            if ((h >= oldh)
+                    || (Math.abs(h - oldh) <= 1 * this.screenHeight / 4)) {
+                if ((h <= oldh)
+                        || (Math.abs(h - oldh) <= 1 * this.screenHeight / 4))
+                    return;
+                this.sizeChanged = false;
+            } else {
+                this.sizeChanged = true;
+            }
+            this.onSizeChangedListenner.onSizeChange(this.sizeChanged, oldh, h);
+            measure(this.width - w + getWidth(), this.height
+                    - h + getHeight());
+        }
+    }
+
+    /**
+     * 设置监听事件
+     *
+     * @param paramonSizeChangedListenner
+     */
+    public void setOnSizeChangedListenner(
+            OnSizeChangedListenner paramonSizeChangedListenner) {
+        this.onSizeChangedListenner = paramonSizeChangedListenner;
+    }
+
+
+    /**
+     * 大小改变的内部接口
+     *
+     * @author junjun
+     */
+    public abstract interface OnSizeChangedListenner {
+        public abstract void onSizeChange(boolean paramBoolean, int w, int h);
+    }
+}
