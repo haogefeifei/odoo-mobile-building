0ad2b12f9ef62dca8f75fa0e614dd45ad7c59a0e
好个飞飞 <haogefeifei@qq.com>
2015-09-18 15:55:02 +0800

    加入百度地图支持

---
+++ baidumap/src/main/java/com/haogefeifei/baidumap/TestMapActivity.java
@@ -0,0 +1,13 @@
+package com.haogefeifei.baidumap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestMapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_map);
+    }
+}
