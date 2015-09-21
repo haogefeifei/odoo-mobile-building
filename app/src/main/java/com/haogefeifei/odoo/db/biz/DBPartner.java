b24aa86c6d3990e89fcba762c2cb3e0a364a659c
好个飞飞 <haogefeifei@qq.com>
2015-09-21 17:57:39 +0800

    db

---
+++ app/src/main/java/com/haogefeifei/odoo/db/biz/DBPartner.java
@@ -0,0 +1,9 @@
+package com.haogefeifei.odoo.db.biz;
+
+/**
+ * 客户数据操作
+ * Created by feifei on 15/9/20.
+ */
+public class DBPartner {
+
+}
---
+++ app/src/main/java/com/haogefeifei/odoo/db/biz/DBVersion.java
@@ -0,0 +1,9 @@
+package com.haogefeifei.odoo.db.biz;
+
+/**
+ * 版本信息数据操作类
+ * Created by feifei on 15/9/20.
+ */
+public class DBVersion {
+
+}
